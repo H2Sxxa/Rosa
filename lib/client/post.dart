fa9900a8d6df74370678349eb21cc517c0e0bbe1
IAXRetailer <h2sxxa0w0@gmail.com>
2023-06-28 06:25:44 +0800

    bin del

--- lib/client/post.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dio/dio.dart';
-
-Future<Response> newnotemclo(String text) {
-  return Dio().postUri(Uri.parse('https://api.mclo.gs/1/log'),
-      data: FormData.fromMap({"content": text}));
-}
--- lib/const.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-//const
-const localizationPath = 'rosa_Data/i18n/';
-const configPath = "rosa_Data/config.json";
-const manifestPath = "rosa_Data/manifest.json";
-
-//final
-final userProfile = "${Platform.environment["USERPROFILE"]}/";
