ce6d48806cb6230df5177cf397a57548185900cb
IAXRetailer <h2sxxa0w0@gmail.com>
2023-07-02 08:24:30 +0800

    finish doc generator

--- lib/const.dart
+++ lib/const.dart
@@ -4,6 +4,6 @@ import 'dart:io';
 const localizationPath = 'rosa_Data/i18n/';
 const configPath = "rosa_Data/config.json";
 const manifestPath = "rosa_Data/manifest.json";
-
+const pluginsPath = "rosa_Data/plugins/";
 //final
 final userProfile = "${Platform.environment["USERPROFILE"]}/";
--- lib/pages/markdown/docgen.dart
+++ lib/pages/markdown/docgen.dart
@@ -1,18 +1,41 @@
+import 'dart:convert';
 import 'dart:io';
 
-import 'package:path/path.dart';
+import 'package:rosa/const.dart';
 
 List<DocContainer> getDocs() {
-  return [];
+  List<DocContainer> result = [];
+  for (var entity in Directory("${pluginsPath}doc").listSync()) {
+    if (Directory(entity.path).statSync().type ==
+        FileSystemEntityType.directory) {
+      if (File("${entity.path}/DOC_REFERENCE.json").existsSync()) {
+        result.add(DocContainer(entity.path));
+      }
+    }
+  }
+
+  return result;
 }
 
 class DocContainer {
   String path;
+  late Map reference;
+  late List<String> sequence;
+  late String docname;
   DocContainer(this.path) {
-    Directory(path);//TODO write here
+    reference = jsonDecode(File("$path/DOC_REFERENCE.json").readAsStringSync());
+    docname = reference["name"];
+    sequence = reference["sequence"];
   }
 
-  String getName() {
-    return basename(path);
+  List<String> getdocspath() {
+    List<String> result = [];
+    for (var i in sequence) {
+      var mdfile = File("$path/$i");
+      if (mdfile.existsSync()) {
+        result.add(mdfile.absolute.path);
+      }
+    }
+    return result;
   }
 }
