15dc6ce0491a8ce2efdd3df7973fed65fa8d040c
IAXRetailer <retailer673@163.com>
2023-07-09 16:25:32 +0800

    update package & Format

--- forgegradle/class/update_manifest.py
+++ forgegradle/class/update_manifest.py
@@ -1,40 +1,50 @@
-import os,json
-from Remilia.res import rPath,rFile
+import json
+import os
+
 from Remilia.log import Logger
+from Remilia.res import rFile, rPath
 
 logger = Logger()
-localls=[pth for pth in os.listdir() if rPath(pth).is_dir()]
+localls = [pth for pth in os.listdir() if rPath(pth).is_dir()]
 for pkg in localls:
-    pkgls=os.listdir(pkg)
-    pkginfo=json.loads(rFile(pkg+"/package.json").text)
-    manifest=pkginfo["manifest"]
-    resultmanifest=[]
-    includels=[]
+    pkgls = os.listdir(pkg)
+    pkginfo = json.loads(rFile(pkg + "/package.json").text)
+    manifest = pkginfo["manifest"]
+    resultmanifest = []
+    includels = []
     for entry in pkgls:
         if rPath(entry).to_file().ext == ".class":
             for entry2 in manifest:
                 if entry == entry2["name"]:
-                    resultmanifest.append({
-                        "name":entry,
-                        "location":entry2["location"],
-                        "uri":{
-                            "blob":"https://github.com/H2Sxxa/Rosa/blob/bin/forgegradle/class/%s/%s"%(pkg,entry),
-                            "raw":"https://github.com/H2Sxxa/Rosa/raw/bin/forgegradle/class/%s/%s"%(pkg,entry)
+                    resultmanifest.append(
+                        {
+                            "name": entry,
+                            "parent": entry2["parent"],
+                            "location": entry2["location"],
+                            "uri": {
+                                "blob": "https://github.com/H2Sxxa/Rosa/blob/bin/forgegradle/class/%s/%s"
+                                % (pkg, entry),
+                                "raw": "https://github.com/H2Sxxa/Rosa/raw/bin/forgegradle/class/%s/%s"
+                                % (pkg, entry),
+                            },
                         }
-                     })
+                    )
                     includels.append(entry)
             if entry not in includels:
-                resultmanifest.append({
-                        "name":entry,
-                        "location":"",
-                        "uri":{
-                            "blob":"https://github.com/H2Sxxa/Rosa/blob/bin/forgegradle/class/%s/%s"%(pkg,entry),
-                            "raw":"https://github.com/H2Sxxa/Rosa/raw/bin/forgegradle/class/%s/%s"%(pkg,entry)
-                        }
-                     })
+                resultmanifest.append(
+                    {
+                        "name": entry,
+                        "parent": "",
+                        "location": "",
+                        "uri": {
+                            "blob": "https://github.com/H2Sxxa/Rosa/blob/bin/forgegradle/class/%s/%s"
+                            % (pkg, entry),
+                            "raw": "https://github.com/H2Sxxa/Rosa/raw/bin/forgegradle/class/%s/%s"
+                            % (pkg, entry),
+                        },
+                    }
+                )
     pkginfo["manifest"] = resultmanifest
-    fin=json.dumps(pkginfo,indent=4)
-    rFile(pkg+"/package.json").write(fin)
-    logger.info("write to",pkg)
-
-
+    fin = json.dumps(pkginfo, indent=4)
+    rFile(pkg + "/package.json").write(fin)
+    logger.info("write to", pkg)
